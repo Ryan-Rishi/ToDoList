924214a7bc70b2b0c1c3574c84895294a62e76b5
Ryan Rishi <51206416+Ryan-Rishi@users.noreply.github.com>
2023-09-12 02:22:32 -0400

    Add files via upload

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ryan Rishi on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
